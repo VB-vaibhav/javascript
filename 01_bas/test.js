5d7aec0dbceeae70cea91ac3413880568a128041
Vaibhav Bhargava <126408883+VB-vaibhav@users.noreply.github.com>
2024-03-12 02:36:32 +0000

    update test.js

--- 01_bas/test.js
+++ 01_bas/test.js
@@ -1,6 +1,7 @@
 console.log("vaibhav");
 let name="vaibhav"
 var price = 53.6
+const PI= 3.14
 console.table([name,price])
 console.log("My first commit done")
-console.log(name+"" +price) 
+console.log(name+" " +price+" "+PI)
